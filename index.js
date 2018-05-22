1c75da9ddf620c1b6ca35744d622daad08a28983
Dita <dita.rahmane.silova@inbox.lv>
2018-05-22 17:56:22 +0300

    radio btn valid

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 // Check if both e-mails are the same
-const eMail1 = document.querySelector('input[name="email"]').value;
-const eMail2 = document.querySelector('input[name="email-again"]').value;
-let value = '';
 
-function checkEmail(theForm) {
-    if(theForm.eMail1 != theForm.eMail2) {
+
+function checkEmail() {
+    const eMail1 = document.querySelector('input[name="email"]').value;
+    const eMail2 = document.querySelector('input[name="email-again"]').value;
+    if(eMail1 != eMail2) {
         alert('Emails  emails don\'t match!');
-        return false;
-    } else {
-        return true;
-    }
+    } 
 }
+
+function validate(form) {
+    let box = document.getElementsByName('design');
+
+    for(let i = 0; i < box.length; ) {
+        if(box[i].checked) {
+            console.log("Hello");
+        }else if( )
+            
+        }
+        i++
+        console.log(i);
+        }
+        
+       
+    return false;
+}
+    
+
+
+
+
+// function validate(form) { 
+//     // Checking if at least one period button is selected. Or not. 
+    
+//     if (!form.design[0].checked && !form.design[1].checked && !form.design[2].checked && !form.design[3].checked){ 
+    
+//     alert("Please Select design"); 
+//     return false;}
+    
+    
+//     if(!document.form1.agree.checked){alert("Please check the terms and conditions");
+//         return false; } 
+    
+    
+//     return true;
+//     }
+
