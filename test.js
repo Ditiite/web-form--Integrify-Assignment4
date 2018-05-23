644000ce2cd3c922c11059a7ca9e31c52c228cbe
Dita Rahmane <dita.rahmane.silova@inbox.lv>
2018-05-23 22:02:59 +0300

    Fix small thinks in validation

--- test.js
+++ test.js
@@ -1,55 +1,148 @@
+
 function validate(myForm) {
+    let isValid = true;
 
     //CHECKS NAME
-    const regName = new RegExp("^[a-zA-Z][a-zA-z\s'-]+");
-    const name = document.getElementsByName('name');
-
-    if (!regName.test(document.myForm.name.value)) {
-        document.getElementById('nameError').innerHTML = "Please check your name";
-        document.myForm.name.focus();
-        return false;
-    } else {
-        document.getElementById('nameError').innerHTML = "";
+    if (!validateName()) {
+        isValid = false;
     }
-
+    
     // CHECKS EMAIL
-    //const regEmail = new RegExp("^(\.?\w+)+@[A-Za-z0-9]+\.[A-Za-z]+$");
-    const eMail1 = document.querySelector('input[name="email"]').value;
-    const eMail2 = document.querySelector('input[name="email-again"]').value;
-
-    if (eMail1 != eMail2) {
-        document.getElementById('emailError').innerHTML = "E-mails should match!";
-        document.myForm.email.focus();
+    if (!validateEmail()) {
+        isValid = false;
     }
 
-    const regEmail = /[\.?\w]+@[A-Za-z0-9]+\.[A-Za-z]+/;
-    if (!regEmail.test(document.myForm.email.value)) {
-        document.getElementById('emailError').innerHTML = "Please check your email pattern";
-        document.myForm.email.focus();
+    // CHECK NUMBER
+    const regNum = /[\d]{6}/;
+    const number = document.myForm.phone;
+    const numErr = document.getElementById('phoneError');
+
+    if(!regNum.test(number.value)) {
+        numErr.innerHTML = "Please provide correct phone number!";
+        number.focus();
     } else {
-        document.getElementById('emailError').innerHTML = "";
+        numErr.innerHTML = "";
+    }
+
+ 
+    // CHECK ADDRESS
+    const regAddress = /[a-zA-Z][\w'\s,-]+/;
+    const address = document.myForm.address;
+    const addressErr = document.getElementById('addressError');
+
+    if(!regAddress.test(address.value)) {
+        addressErr.innerHTML = "Please check your address!";
+        address.focus();
+    } else {
+        addressErr.innerHTML = "";
+    }
+
+    // CHECK CITY
+    const regCity = /[a-zA-Z][\w\s'-]+/;
+    const city = document.myForm.city;
+    const cityErr = document.getElementById('cityError');
+
+    if(!regCity.test(city.value)) {
+        cityErr.innerHTML = "Please select correct city!";
+        city.focus();
+    } else {
+        cityErr.innerHTML = "";
+    }
+
+
+    // COUNTRY
+    const regCountry = /[a-zA-Z][\w\s'-]+/;
+    const country = document.myForm.country;
+    const countryErr = document.getElementById('countryError');
+
+    if (!regCountry.test(country.value)) {
+        countryErr.innerHTML = "Please provide your country!";
+        country.focus();
+    } else {
+        countryErr.innerHTML = ""; 
     }
 
 
     // CHECKS ZIP CODE
-    const zip = document.getElementsByName('zip');
     const zipReg = /[\w-\/]{3,9}/;
+    const zip = document.myForm.zip;
+    const zipErr = document.getElementById('zipError');
 
-    if (!zipReg.test(document.myForm.zip.value)) {
-        document.getElementById('zipError').innerHTML = "Please check your zip code.";
-        document.myForm.zip.focus();
+    if (!zipReg.test(zip.value)) {
+        zipErr.innerHTML = "Please check your zip code.";
+        zip.focus();
     } else {
-        zipError.innerHTML = "";
+        zipErr.innerHTML = "";
     }
 
-    // COUNTRY
-    if (document.myForm.country.value == "") {
-        alert("Please provide your country!");
+    
+    // CHECK LINK
+    const regLink = /(^(http[s]?:\/\/)?|([w]{3}\.))?([\/\w.=?:;"!&-]+)/;
+    const link = document.myForm.link;
+    const linkErr = document.getElementById('linkError');
+
+    if(!regLink.test(link.value)) {
+        linkErr.innerHTML = "Please provide portfolio link!";
+        link.focus();
+    } else {
+        linkErr.innerHTML = "";
     }
-
-    validateBox();
-
+    
+    if (!validateBox()) {
+        isValid = false;
+    }
+    
+    return isValid;
 }
+
+function validateEmail() {
+    let isValid = true;
+
+    //const regEmail = new RegExp("^(\.?\w+)+@[A-Za-z0-9]+\.[A-Za-z]+$");
+    const eMail1 = document.querySelector('input[name="email"]').value;
+    const eMail2 = document.querySelector('input[name="email-again"]').value;
+    const eMailErr =  document.getElementById('emailError');
+    const eMailErr2 =  document.getElementById('emailError2');
+    const email = document.myForm.email;
+
+    if (eMail1 !== eMail2) {
+        eMailErr2.innerHTML = "E-mails should match!";
+        document.myForm.email.focus();
+        isValid = false;
+    } else {
+        eMailErr2.innerHTML = "";
+    }
+
+    const regEmail = /[\.?\w]+@[A-Za-z0-9]+\.[A-Za-z]+/;
+    if (!regEmail.test(email.value)) {
+        eMailErr.innerHTML = "Please check your email pattern";
+        email.focus();
+        isValid = false;
+    } else {
+        eMailErr.innerHTML = "";
+    }
+
+    return isValid;
+}
+
+function validateName() {
+    let isValid = true;
+
+    const regName = /^[a-zA-Z][a-zA-z\s'-]+/;
+    const name = document.myForm.name;
+    const nameErr = document.getElementById('nameError');
+
+    if (!regName.test(name.value)) {
+        nameErr.innerHTML = "Please check your name";
+        name.focus();
+        isValid = false;
+    } else {
+        nameErr.innerHTML = "";
+    }
+
+    return isValid;
+}
+
 
 function validateBox() {
     // Check radio buttons
@@ -101,55 +194,5 @@ function validateBox() {
     }
 };
 
-
-
-
-// function validate(myForm) {
-//     checkEmail();
-//     validateButtons();
-//     phone();
-// }
-
-// function phone() {
-//     const phone = document.querySelector('input[name="phone"]').value;
-//     const regex = /[\d]{6}/;
-
-//     if (regex.test(phone)) {
-//         console.log('Wrong patern');
-//         phone.classList.add('error');
-//         alert('wrong');
-//     }
-// }
-
-
-// // Check email
-// function checkEmail() {
-//     const eMail1 = document.querySelector('input[name="email"]').value;
-//     const eMail2 = document.querySelector('input[name="email-again"]').value;
-//     if(eMail1 != eMail2) {
-//         alert('Emails  emails don\'t match!');
-//         return false;
-//     } 
-// };
-
-// // Radio buttons and checkbox
-// function validateButtons() {
-//     //form.preventDafault();
-
-//     // Check radio buttons
-//     const targetEL = document.getElementsByName('design');
-//     let count = 0;
-//     let countCheck = 0;  
-
-//     for(let i = 0; i < targetEL.length; i++) {
-//         if(targetEL[i].checked) {
-//             count++;
-//             break;
-//         }  
-//     }
-//     // Check checkbox
-//     const checkboxEl = document.getElementsByName('check');
-
-//    
 
 
